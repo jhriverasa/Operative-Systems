7fead55a18515c077d86746f147466d2e5085bf1
jhriverasa <jhriverasa@unal.edu.co>
2018-03-19 13:03:26 -0500

    Update structgenerator.c

--- Practice1/structgenerator.c
+++ Practice1/structgenerator.c
@@ -3,18 +3,21 @@
 #include <time.h>
 #include <string.h>
 
+
 struct dogType{
 	int age;
 	float weight;
 	char gender;
-	int height; //cm
+	int height;  //cm
 	char name[32];
 	char animal_type[32];
 	char race[16];
 };
 
-
+//# names in petnames.txt
 const int n= 1716;
+
+
 //read petnames.txt and return k-th line.
 char* get_name(int k){
 	FILE *file;
@@ -33,6 +36,8 @@ char* get_name(int k){
 	return ptr;
 }
 
+
+//return a random integer between a and b
 int randint(int a,int b){
 	int r;
 
@@ -48,14 +53,40 @@ int randint(int a,int b){
 
 }
 
+//return a random float between min and max
 float randfloat( float min, float max ){
     float scale = rand() / (float) RAND_MAX;
     return min + scale * ( max - min );     
 }
 
-char* randname(char** names){
-    return names[randint(0,n-1)];
+
+void writeStruct(void *reg){
+	FILE *file;
+	struct dogType *dt;
+	dt = reg;
+
+	file = fopen("dataDogs.txt","wb+");
+	fwrite(&dt,sizeof(struct dogType),1,file);
+	fclose(file);
+	
 }
+
+//return Dan Bernstein Hash Function with modulo operation.
+unsigned long hash(unsigned char *str, int mod){
+
+	unsigned long hash = 5381;
+	int c;
+
+	while(c= *str++){
+		hash = ((hash << 5) + hash) + c;
+	}
+	return hash % mod;
+
+}
+
+
+
+
 
 
 int main(){
@@ -63,37 +94,46 @@ int main(){
 	long i;
 	FILE *file;
 	file = fopen("dataDogs.txt","w+");
-	int antirepeat = 151;
-	
-    int a=2;
-    int b=16;
-    float min= 5;
-    float max= 50;
+
+	int antirepeat = 151; //arbitrary prime number
     char atype[32] = "12345678901234567890123456789012";
     char arace[16] = "1234567890123456";
 
-   
-    printf("Random int %d\n", randint(a,b));
-    printf("Random float %f\n", randfloat(min,max));
+
     for(i=0;i<100;i++){ // put it equals 10 M (now 100 for testing)
+
+    	//build and fill dogType Struct with pseudo-random data
     	struct dogType *reg;
     	reg = malloc(sizeof(struct dogType));
     	reg->age = randint(0,20);
 		reg->weight = randfloat(1.0,5.0);
 		reg->height = randint(30,200);
-
-		antirepeat =  (antirepeat*19+i) % n +1;
-
+		antirepeat =  (antirepeat*19+i) % n +1; 
 		strcpy(reg->name,get_name(antirepeat));
 		strcpy(reg->animal_type, atype);
 		strcpy(reg->race, "1234567890123456");
+
     	
 		//here we have reg ready to write into file.
+		
 
-    	//fputs(get_name(antirepeat),file);
-   		
+		//printf("%lu\n", hash(reg->name,10) );
+		delme[hash(reg->name,10)]++; 
 
+		if(i==0){
+			// printf("%d\n",reg->age );
+			// printf("%f\n",reg->weight );
+			// printf("%d\n",reg->height );
+			// printf("%s\n",reg->name );
+			// printf("%s\n",reg->animal_type );
+			// printf("%s\n",reg->race );
+			writeStruct(reg);	
+		}
+
+
+    	
     }
+
 
     
     return 0;
